2f9b9588f3006152438b8955d5dc2b5960a3aae9
Oleg Izmerly <oizmerly@splunk.com>
2022-07-24 16:09:18 -0500

    publish

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 "use strict";
+class BoardScene extends Phaser.Scene {
+    constructor() {
+        super('board');
+    }
+    preload() {
+        this.load.image('tile', './assets/graphics/tile.png');
+    }
+    create() {
+        for (let x = 0; x < 4; x++)
+            for (let y = 0; y < 6; y++) {
+                if (x == 2 && y == 2)
+                    continue;
+                this.add.image(x * 250, y * 250, 'tile')
+                    .setOrigin(0, 0).setDisplaySize(250, 250);
+                //
+                // if (Math.random() < 0.2)
+                //     this.add.image(x * 250, y * 250, 'ball').setOrigin(0, 0).setDisplaySize(250, 250);
+            }
+    }
+}
 /**
  * The game is loading scene
  */
@@ -8,6 +28,7 @@ class LoadingScene extends Phaser.Scene {
     }
     create() {
         this.add.text(0, 0, 'Loading ...', { fontFamily: 'verdana', fontSize: '2em' });
+        this.scene.start('board');
     }
 }
 /**
@@ -23,7 +44,8 @@ class TheGame extends Phaser.Game {
                 parent: 'screen',
             },
             scene: [
-                LoadingScene
+                LoadingScene,
+                BoardScene,
             ]
         });
     }
